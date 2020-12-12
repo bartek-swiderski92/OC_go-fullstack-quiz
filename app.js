cf0c7fd842d905d7424761f1f91af3292d56f90b
Phos23 <phos23@gmail.com>
2020-12-12 13:43:22 +0000

    fix get and get :id

--- app.js
+++ app.js
@@ -49,8 +49,10 @@ app.post('/api/products', (req, res, next) => {
 app.get('/api/products/:id', (req, res, next) => {
     Product.findOne({
         _id: req.params.id
-    }).then((thing) => {
-        res.status(200).json(thing);
+    }).then((product) => {
+        res.status(200).json({
+            product
+        });
     }).catch((error) => {
         res.status(404).json({
             error: error
@@ -60,7 +62,9 @@ app.get('/api/products/:id', (req, res, next) => {
 
 app.get('/api/products', (req, res, next) => {
     Product.find().then((products) => {
-        res.status(200).json(products);
+        res.status(200).json({
+            products
+        });
     }).catch((error) => {
         res.status(400).json({
             error: error
